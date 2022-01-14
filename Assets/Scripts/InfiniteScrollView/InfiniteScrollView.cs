ba51feeea495d11e8fb66e5a8c2b35c5c0bf6825
DESKTOP-3625S7R\brkna <brkn.aknc4@gmail.com>
2022-01-14 07:29:10 +0300

    Add_ScrollView

---
+++ Assets/Scripts/InfiniteScrollView/InfiniteScrollView.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InfiniteScrollView : MonoBehaviour, IBeginDragHandler, IDragHandler, IScrollHandler
+{
+    [SerializeField] private ScrollContent scrollContent;
+    private ScrollRect scrollRect;
+    private Vector2 lastDragPosition;
+    [SerializeField] private bool positiveDrag = true;
+    private Transform tempTransformImage, spawnedTransform;
+    private Camera mainCam;
+    private void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+        scrollRect.vertical = scrollContent.Vertical;
+
+        tempTransformImage = scrollContent.transform.GetChild(scrollContent.transform.childCount - 1);
+
+        mainCam = Camera.main;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        lastDragPosition = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        positiveDrag = eventData.position.y > lastDragPosition.y;
+
+        VisibleControl();
+
+        lastDragPosition = eventData.position;
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        if (scrollContent.Vertical)
+        {
+            positiveDrag = eventData.scrollDelta.y > 0;
+        }
+        else
+        {
+            positiveDrag = eventData.scrollDelta.y < 0;
+        }
+    }
+
+    private void VisibleControl()
+    {
+        if (!tempTransformImage.GetComponent<RectTransform>().IsVisibleFrom(mainCam))
+        {
+            spawnedTransform = ObjectPool.Instance.SpawnScrollObject(tempTransformImage.gameObject, scrollContent.transform);
+        }
+        switch (positiveDrag)
+        {
+            case true:
+                if (spawnedTransform != null)
+                {
+                    spawnedTransform.SetSiblingIndex(0);
+                    scrollContent.ContentAlign();
+
+                    spawnedTransform = null;
+                }
+                if (tempTransformImage != scrollContent.transform.GetChild(scrollContent.transform.childCount - 1))
+                    tempTransformImage = scrollContent.transform.GetChild(scrollContent.transform.childCount - 1);
+                break;
+            case false:
+                if (spawnedTransform != null)
+                {
+                    spawnedTransform.SetSiblingIndex(scrollContent.transform.childCount - 1);
+                    scrollContent.ContentAlign();
+
+                    spawnedTransform = null;
+                }
+                if (tempTransformImage != scrollContent.transform.GetChild(0))
+                    tempTransformImage = scrollContent.transform.GetChild(0);
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectPool : MonoBehaviour
+{
+    private static ObjectPool instance = null;
+    public static ObjectPool Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("ObjectPool").AddComponent<ObjectPool>();
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        instance = this;
+    }
+    [SerializeField] private List<GameObject> scrollSoldierPool = new List<GameObject>();
+    [SerializeField] private GameObject scrollSoldiers;
+    [SerializeField] private List<GameObject> scrollBarrackPool = new List<GameObject>();
+    [SerializeField] private GameObject scrollBarrack;
+    [SerializeField] private List<GameObject> scrollPowerPlantPool = new List<GameObject>();
+    [SerializeField] private GameObject scrollPowerPlant;
+    private Transform tempTransform;
+    [SerializeField] private GameObject tempGameObject;
+
+    public Transform SpawnScrollObject(GameObject _scrollObject, Transform scrollContent)
+    {
+        tempGameObject = _scrollObject;
+
+        if (_scrollObject == scrollBarrack)
+        {
+            if (scrollBarrackPool.Count > 0)
+            {
+                tempTransform = scrollBarrackPool[0].transform;
+                scrollBarrackPool[0].gameObject.SetActive(true);
+                scrollBarrackPool[0].transform.SetParent(scrollContent);
+                scrollBarrackPool.Remove(scrollBarrackPool[0]);
+                scrollBarrackPool.Add(_scrollObject);
+            }
+            else
+            {
+                tempTransform = Instantiate(scrollBarrack, scrollContent).transform;
+                scrollBarrackPool.Add(_scrollObject);
+            }
+        }
+        if (_scrollObject == scrollPowerPlant)
+        {
+            if (scrollPowerPlantPool.Count > 0)
+            {
+                tempTransform = scrollPowerPlantPool[0].transform;
+                scrollPowerPlantPool[0].gameObject.SetActive(true);
+                scrollPowerPlantPool[0].transform.SetParent(scrollContent);
+                scrollPowerPlantPool.Remove(scrollPowerPlantPool[0]);
+                scrollPowerPlantPool.Add(_scrollObject);
+            }
+            else
+            {
+                tempTransform = Instantiate(scrollPowerPlant, scrollContent).transform;
+                scrollPowerPlantPool.Add(_scrollObject);
+            }
+        }
+        else
+        {
+            if (scrollSoldierPool.Count > 0)
+            {
+                tempTransform = scrollSoldierPool[0].transform;
+                scrollSoldierPool[0].gameObject.SetActive(true);
+                scrollSoldierPool[0].transform.SetParent(scrollContent);
+                scrollSoldierPool.Remove(scrollSoldierPool[0]);
+                scrollSoldierPool.Add(_scrollObject);
+            }
+            else
+            {
+                tempTransform = Instantiate(scrollSoldiers, scrollContent).transform;
+                scrollSoldierPool.Add(_scrollObject);
+            }
+        }
+
+        _scrollObject.transform.SetParent(null);
+        _scrollObject.SetActive(false);
+
+        return tempTransform;
+    }
+}
